3be045267980f6d820b8e0d7fbfadeda94405524
brandonp129 <155605027+brandonp129@users.noreply.github.com>
2024-10-01 20:07:34 -0400

    start of chess gmae

---
+++ src/com/chess/engine/board/Tile.java
@@ -0,0 +1,71 @@
+package com.chess.engine.board;
+
+
+import com.chess.engine.pieces.Piece;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class Tile {
+
+
+    protected final int tileCoordinate;
+    private static final Map<Integer, EmptyTile> EMPTY_TILES = createdAllPossibleEmptyTiles();
+
+    private static Map<Integer, EmptyTile> createdAllPossibleEmptyTiles() {
+
+        final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
+
+        for(int i = 0; i < 64; i++) {
+            emptyTileMap.put(i, new EmptyTile(i));
+        }
+
+        return ImmutableMap.copyOf(emptyTileMap);
+
+    }
+
+
+    Tile(int tileCoordinate) {
+        this.tileCoordinate = tileCoordinate;
+    }
+
+    public abstract boolean isTileOccupied();
+
+    public abstract Piece getPiece();
+
+    public static final class EmptyTile extends Tile{
+
+        EmptyTile(int coordinate) {
+            super(coordinate);
+        }
+
+        @Override
+        public boolean isTileOccupied() {
+            return false;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return null;
+        }
+    }
+
+    public static final class OccupiedTile extends Tile {
+        private final Piece pieceOnTile;
+
+        OccupiedTile(int tileCoordinate, Piece pieceOnTile) {
+            super(tileCoordinate);
+            this.pieceOnTile = pieceOnTile;
+        }
+
+        @Override
+        public boolean isTileOccupied() {
+            return true;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return this.pieceOnTile;
+        }
+    }
+}
