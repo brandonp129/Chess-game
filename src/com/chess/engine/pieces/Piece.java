8a90d14a8ac2cab52a067cd49ee0e1d8ace2ac5b
brandonp129 <155605027+brandonp129@users.noreply.github.com>
2024-10-04 12:08:17 -0400

    starting to develop pawn methods

--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -11,13 +11,20 @@ import java.util.Collection;
 public class Piece {
 
     protected final int piecePosition;
-
     protected final Alliance pieceAlliance;
+    protected final boolean isFirstMove;
+
 
     Piece(final int piecePosition, final Alliance pieceAlliance) {
 
         this.pieceAlliance = pieceAlliance;
         this.piecePosition = piecePosition;
+        //TODO there is more work to do here
+        this.isFirstMove = false;
+    }
+
+    public boolean isFirstMove() {
+        return this.isFirstMove;
     }
 
     public Alliance getPieceAlliance() {
